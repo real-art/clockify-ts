4802bb94e5a8f420401a31eb58175f9c03fd9232
Gary Riggins <12747822+gary-riggins@users.noreply.github.com>
2024-05-20 15:04:59 -0400

    Give detailed report time entries their own named type

--- dist/esm/Types/DetailedReportType.d.ts
+++ dist/esm/Types/DetailedReportType.d.ts
@@ -5,39 +5,40 @@ type DetailedReportType = {
         entriesCount: number;
         totalAmount: number;
     }[];
-    timeentries: {
-        _id: string;
-        description: string;
-        userId: string;
-        billable: boolean;
-        taskId: string;
-        projectId: string;
-        clientId: string;
-        timeInterval: {
-            start: Date;
-            end: Date;
-            duration: number;
-        };
-        approvalRequestId: string;
-        taskName: string;
-        tags: string[];
-        isLocked: boolean;
-        customFields: {
-            customFieldId: string;
-            value: string;
-        }[];
-        invoicingInfo: {
-            invoiceId: string;
-            invoiceNumber: string;
-            manuallyInvoiced: boolean;
-        };
-        amount: number;
-        rate: number;
-        userName: string;
-        userEmail: string;
-        projectName: string;
-        projectColor: string;
-        clientName: string;
-    }[];
+    timeentries: DetailedReportTimeEntryType[];
 };
-export { DetailedReportType, };
+type DetailedReportTimeEntryType = {
+    _id: string;
+    description: string;
+    userId: string;
+    billable: boolean;
+    taskId: string;
+    projectId: string;
+    clientId: string;
+    timeInterval: {
+        start: Date;
+        end: Date;
+        duration: number;
+    };
+    approvalRequestId: string;
+    taskName: string;
+    tags: string[];
+    isLocked: boolean;
+    customFields: {
+        customFieldId: string;
+        value: string;
+    }[];
+    invoicingInfo: {
+        invoiceId: string;
+        invoiceNumber: string;
+        manuallyInvoiced: boolean;
+    };
+    amount: number;
+    rate: number;
+    userName: string;
+    userEmail: string;
+    projectName: string;
+    projectColor: string;
+    clientName: string;
+};
+export { DetailedReportType, DetailedReportTimeEntryType };
